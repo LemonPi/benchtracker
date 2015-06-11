76363036dc4cbb7c2367c37a9d9a182cfa763161
Johnson Zhong <johnson9510@hotmail.com>
2015-06-11 18:16:33 -0500

    Filter building partially complete (still lacks final packaging into get request for data)

--- static/viewer.js
+++ static/viewer.js
@@ -1,103 +1,297 @@
 // globals
 var root_url = 'http://localhost:5000'
 var tasks = new Set();
+var task_cached = "";
 var params = [];
 var labels = [];
+var active_querying_filter;
+// serialize without [] for array parameters
+jQuery.ajaxSettings.traditional = true; 
+
 
 // DOM modifying functions
 $(document).ready(function() {
 
 // update all other selection boxes if necessary when tasks are changed
-$("#task_select").on('change', ':checkbox', function() {
-	if (this.checked) tasks.add(this.value);
-	else tasks.delete(this.value);
-	update_params();
+var checkboxes = document.getElementById("task_select").getElementsByTagName("input");
+for (var i=0; i < checkboxes.length; ++i) {
+	checkboxes[i].addEventListener('change', function() {
+		task_cached = "";
+		if (this.checked) tasks.add(this.value);
+		else tasks.delete(this.value);
+		update_params();
+	});
+}
+
+var add_filter = document.getElementById("add_filter");
+add_filter.addEventListener('click', create_filter_window);
+
+var gen_plot = document.getElementById('generate_plot');
+gen_plot.addEventListener('click', generate_plot);
+
+
 });
 
-$("#add_filter").on('click', null, function() {
-	alert("adding filter");
-});
 
 
-});
-
-
-// utility functions
+// actions upon selecting a task
 function update_params() {
+	remove_all_filters();
 	if (tasks.size === 0) {
 		params = [];
-		console.log("cleared params");
+		add_filter.style.visibility = 'hidden';
+		report_debug("cleared params");
 		populate_param_windows();
 	}
 	else {
 		$.getJSON(root_url+'/tasks/?'+create_task_query(), get_params);
-		console.log("updated params");
+		add_filter.style.visibility = 'visible';
+		report_debug("updated params");
 	}
 	// to assure correct sequence, populate param windows must be called inside the functions that update params
-
+	report_debug("---------------");
 }
-
-function get_params( data ) {
-	if (data.status !== "OK") {
-		alert(data.status);
-		return;
-	}
-	params = data.params;
-	console.log("got params");
-	populate_param_windows();
-}
-
-// populate x and y parameter windows
 function populate_param_windows() {
 	$('#x_param').empty();
 	$('#y_param').empty();
-	fill_labels();
-
-	fill_selects('x_param');
-	fill_selects('y_param');
-	console.log("populated param windows");
-}
-
-// parameter labels are shared by x,y, and filters
-function fill_labels() {
-	labels = [];
+	var x_param = document.getElementById('x_param');
+	var y_param = document.getElementById('y_param');
 	for (p of params) {
 		p_pair = p.split(' ');
 		var label = document.createElement('label');
+		var input = document.createElement('input');
+		input.type = 'radio';
+		input.value = p;
+		input.name = "param";
 		label.title = p_pair[1];
+
+		label.appendChild(input);
 		label.appendChild(document.createTextNode(p_pair[0]));
 		label.appendChild(document.createElement('br'));
-		labels.push(label);
+		x_param.appendChild(label);
+		label_y = label.cloneNode(true);
+		y_param.appendChild(label_y);
+	}
+	report_debug("populated param windows");
+}
+
+// actions upon adding a filter
+function create_filter_window() {
+	var sel_bar = document.getElementById('selection_bar');
+	var filter = document.createElement('div');
+	filter.className = "filter selection_box";
+
+	// create selection menu
+	var param_texts = [];
+	for (p of params) param_texts.push(p.split(' ')[0]);
+	var select_param = create_selection(param_texts, params, "--- parameters ---");
+	select_param.className = "filter_param";
+	select_param.addEventListener('change', query_param, false);
+
+	// create close button
+	var close = document.createElement('a');
+	close.className = "close attractive_button";
+	close.text = 'delete filter';
+	close.addEventListener('click', remove_filter, false);
+
+	filter.appendChild(select_param);
+	filter.appendChild(close);
+	sel_bar.insertBefore(filter, add_filter);
+}
+
+// actions upon generating a plot
+function generate_plot() {
+	var x = document.querySelector('#x_param input[name="param"]:checked');
+	var y = document.querySelector('#y_param input[name="param"]:checked');
+	if (!x) {report_error("x parameter not selected"); return;}
+	if (!y) {report_error("y parameter not selected"); return;}
+	report_debug("x_param: " + x.value);
+	report_debug("y_param: " + y.value);
+
+	var sel_bar = document.getElementById('selection_bar');
+	filters = sel_bar.getElementsByClassName('filter');
+	for (var f = 0, len = filters.length; f < len; ++f) {
+		var filtered_param = filters[f].getElementsByClassName('filter_param');
+		if (filtered_param.length > 0) {
+			filtered_param = filtered_param[0].text;
+			report_debug("fp: " + filtered_param);
+		}
+
 	}
 }
 
-// fill the labels with the actual selections
-function fill_selects(select_name) {
-	var param_len = params.length;
-	var select_box = document.getElementById(select_name);
 
-	for (var p = 0; p < param_len; ++p) {
-		// new label
-		var label = labels[p].cloneNode(true);
-		var select = document.createElement('input');
-		select.type = "radio";
-		// select name might be unncessary (since enclosed by select box with same id)
-		// select.name = select_name;
-		select.value = params[p];
-		// selection should be before text
-		label.insertBefore(select, label.firstChild);
 
-		select_box.appendChild(label);		
+// JSON request callbacks
+function get_params( data ) {
+	if (data.status !== "OK") {
+		report_error(data.status);
+		return;
+	}
+	params = data.params;
+	report_debug("got params");
+	populate_param_windows();
+}
+
+// after selecting a parameter, describe its properties
+function describe_param( data ) {
+	if (data.status !== "OK") {
+		report_error(data.status);
+		return;
+	}
+	clean_querying_filter(data);
+
+	// add a selection if it doesn't exist
+	if (active_querying_filter.getElementsByClassName('filter_sel').length === 0) {
+		var data_type = document.createElement('select');
+		data_type.className = "filter_sel";
+
+		var categorical = document.createElement('option');
+		var range = document.createElement('option');
+
+		categorical.value = categorical.text = 'categorical';
+		range.value = range.text = 'range';
+
+		if (data.type === "categorical") categorical.selected = "selected";
+		else if (data.type === "range") range.selected = "selected";
+
+		data_type.appendChild(categorical);
+		data_type.appendChild(range);
+
+		data_type.addEventListener('change', function() {
+			active_querying_filter = this.parentNode;
+			mode = this.value;
+			var query_param = active_querying_filter.getElementsByClassName("filter_param")[0].value;
+			report_debug(query_param);
+			$.getJSON([root_url,'/param?',create_task_query(),'&p=',query_param,'&m=',mode].join(""), 
+				describe_param_adjust, false );
+		});
+
+		active_querying_filter.appendChild(data_type);
+	}
+	// need to update event listener
+	else {
+		var data_type = active_querying_filter.getElementsByClassName('filter_sel')[0];
+
+	}
+
+	// fill in either a range or a multi-select
+	create_filter_val(active_querying_filter, data);
+	// query completed
+	active_querying_filter = "";
+}
+
+// update existing elements
+function describe_param_adjust( data ) {
+	if (data.status !== "OK") {
+		report_error(data.status);
+		return;
+	}
+	clean_querying_filter(data);
+
+	report_debug("adjusting parameter query");
+
+	create_filter_val(active_querying_filter, data);
+	// query adjustment completed
+	active_querying_filter = "";
+}
+
+// remove old elements inside active querying filter (to be updated)
+function clean_querying_filter( data ) {
+	var old_filter_vals = active_querying_filter.getElementsByClassName("filter_val");
+	while (old_filter_vals[0]) active_querying_filter.removeChild(old_filter_vals[0]);
+
+	var filter_sel = active_querying_filter.getElementsByClassName("filter_sel");
+	if (filter_sel.length > 0) {
+		report_debug(filter_sel.length);
+		filter_sel[0].value = data.type;
 	}
 }
 
+function create_filter_val(target, data) {
+	// create either 2 selection boxes for min and max or a multi-select for in filter
+	var filter_val = document.createElement('div');
+	filter_val.className = "filter_val";
+	if (data.type === "categorical") {
+		var select_in = create_selection(data.val, data.val, "ctrl+click for multiple, shift+click for range", true);
+		filter_val.appendChild(select_in);
+	}
+	else if (data.type === "range") {
+		var min = document.createElement('input');
+		var max = document.createElement('input');
+		min.type = max.type = "text";
+		min.name = "min";
+		max.name = "max";
+		min.placeholder = min.min = data.val[0];
+		max.placeholder = max.max = data.val[1];
+		filter_val.appendChild(min);
+		filter_val.appendChild(max);
+	}	
+	target.appendChild(filter_val);
+}
+
+
+
+
+
+// utility functions
+// always attached to a close button inside the filter
+function remove_filter() {
+	console.log(this.parentNode);
+	var filter = this.parentNode;
+	filter.parentNode.removeChild(filter);
+}
+
+function remove_all_filters() {
+	var filters = document.getElementsByClassName('filter');
+	while (filters[0]) {
+		filters[0].parentNode.removeChild(filters[0]);
+	}
+}
+
+function query_param() {
+	var param = this.value;
+	active_querying_filter = this.parentNode;
+	$.getJSON([root_url,'/param?',create_task_query(),'&p=',param].join(""), describe_param, false );
+	report_debug(this.value);
+}
+
+function create_selection(texts, values, prompt_text, multiple=false) {
+	var select = document.createElement('select');
+	if (multiple) select.multiple = true;
+	var prompt = document.createElement('option');
+	prompt.disabled = "disabled";
+	prompt.selected = "selected";
+	prompt.text = prompt_text;
+	prompt.value = "";
+	select.appendChild(prompt);
+	for (var i = 0, len = values.length; i < len; ++i) {
+		var opt = document.createElement('option');
+		opt.value = values[i];
+		opt.text = texts[i];
+		select.appendChild(opt);
+	}	
+	return select;
+}
 function create_task_query() {
-	var qs = "";
-	for (task of tasks) 
-		qs += "t=" + encodeURIComponent(task) + "&";	
-	if (qs.length > 0) 
-		qs = qs.substring(0, qs.length - 1);	// chop last & (mostly harmless)
+	if (!task_cached) {
+		var qs = [];
+		for (task of tasks) 
+			qs.push("t=",encodeURIComponent(task),"&");	
+		if (qs.length > 0) 
+			qs.pop();	// chop last & (mostly harmless)
 
-	return qs;
+		task_cached = qs.join("");
+	}
+	else {
+		report_debug("task cached");
+	}
+	return task_cached;
 }
 
+function report_error(error) {
+	alert(error);
+}
+function report_debug(debug) {
+	console.log(debug)
+}
